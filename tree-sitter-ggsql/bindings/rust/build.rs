f513ffe908449f7f615203d86af62e03585f88a4
Teun van den Brand <tahvdbrand@gmail.com>
2026-01-09 14:54:46 +0100

    add grammar regeneration to build script

--- tree-sitter-ggsql/bindings/rust/build.rs
+++ tree-sitter-ggsql/bindings/rust/build.rs
@@ -1,6 +1,33 @@
 use std::path::PathBuf;
+use std::process::Command;
 
 fn main() {
+    let check = Command::new("tree-sitter").arg("--version").output();
+    match check {
+        Ok(output) if output.status.success() => {}
+        _ => {
+            println!("tree-sitter-cli not found. Attempting to install...");
+            let installation = Command::new("npm")
+                .args(["install", "-g", "tree-sitter-cli"])
+                .status()
+                .expect("Failed to install tree-sitter-cli");
+
+            if !installation.success() {
+                eprintln!("Failed to install tree-sitter-cli");
+                std::process::exit(1)
+            }
+        }
+    }
+
+    let regenerate = Command::new("tree-sitter")
+        .arg("generate")
+        .status()
+        .expect("Failed to regenerate tree sitter grammar.");
+
+    if !regenerate.success() {
+        eprintln!("Failed to regenerate tree sitter grammar.");
+    }
+
     let dir: PathBuf = ["src"].iter().collect();
 
     cc::Build::new()
